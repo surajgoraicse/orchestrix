192c813c01a58cdd0d1b3c6cad6109c6b1aa58b8
surajgoraicse <surajgoraicse@gmail.com>
2026-07-17 17:26:20 +0530

    [scheduler] remove sqlc from git

--- services/scheduler/internal/db/query/tasks.sql
+++ services/scheduler/internal/db/query/tasks.sql
@@ -4,3 +4,17 @@ INSERT INTO tasks(task, scheduled_at)
 RETURNING
     id;
 
+-- name: GetPendingTasks :many
+SELECT id, task FROM tasks WHERE scheduled_at < (NOW() + INTERVAL '30 seconds') AND picked_at IS NULL ORDER BY scheduled_at FOR UPDATE SKIP LOCKED;
+
+-- name: MarkTaskAsPicked :exec
+UPDATE tasks SET picked_at = NOW() WHERE id = $1;
+
+-- name: UpdateTaskStarted :exec
+UPDATE tasks SET started_at = $1, error = $2 WHERE id = $3;
+
+-- name: UpdateTaskCompleted :exec
+UPDATE tasks SET completed_at = $1, error = $2 WHERE id = $3;
+
+-- name: UpdateTaskFailed :exec
+UPDATE tasks SET failed_at = $1, error = $2 WHERE id = $3;
